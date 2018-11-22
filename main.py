92558ad789f0b2aa39116a14ab21ea5238ba731a
Code2Joke <codejoke@web.de>
2018-11-22 20:57:09 +0100

    Add main.py and general.html for the text objects

---
+++ main.py
@@ -0,0 +1,5 @@
+import webview
+
+if __name__ == '__main__':
+    # Create a standard webview window
+    webview.create_window('Quoties for Soulies', resizable=True, fullscreen=True, background_color='#2D2D2D')
